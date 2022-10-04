44a5abcfb98ba83a7486918d46d39b32b89ffe9a
Polly <pollysha23@gmail.com>
2022-10-04 22:44:43 +0300

    my program added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] InputSrtingArray ()
+{
+    Console.Write($"Введите количество элементов массива: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    while (n <= 0)
+    {
+        Console.Write($"Введите значение больше нуля ");
+        n = Convert.ToInt32(Console.ReadLine());
+    }
+    string [] stringArray = new string [n];
+    for (int i=0; i<n; i++)
+    {
+        Console.Write($"Введите {i+1}-й элемент строчного массива: ");
+        stringArray[i] = Console.ReadLine(); 
+    }
+    return stringArray;
+}
+    
+void ShowStringArray (string [] array)
+{
+    for (int i=0; i<array.Length; i++)
+        Console.Write(array[i]+" | ");
+    Console.WriteLine();
+}
+
+int FindSizeForNewArray (string [] array,int count)
+{
+    int size = 0;
+    for (int i=0; i< array.Length; i++)
+        if (array[i].Length <= count) size++;
+    return size;
+}
+
+string [] CreateArrayOfShortStrings (string [] array, int size, int count)
+{   
+    string [] shortArray = new string [size];
+    int j=0;
+        for (int i=0; i< array.Length; i++)
+            if (array[i].Length <= count) 
+            {
+                shortArray[j] = array[i];
+                j++;
+            }
+        return shortArray;
+}
+
+string[] initialArray = InputSrtingArray();
+ShowStringArray(initialArray);
+Console.Write($"Введите максимальную длинну элемента в новом массиве: ");
+int count= Convert.ToInt32(Console.ReadLine());
+int size = FindSizeForNewArray(initialArray,count);
+if (size >0) 
+{
+    string [] newArray = CreateArrayOfShortStrings(initialArray,size,count);
+    ShowStringArray(newArray);
+}
+else 
+Console.Write($"В исходном массиве отсутсвуют элементы, соотвествующие условию");
